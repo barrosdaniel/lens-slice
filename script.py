f1950de481cb9cb06c18684ff4b248e6d2eca7fb
barrosdaniel <barros.daniel@outlook.com>
2023-11-02 17:03:23 +0800

    feature: Complete Make Some Pizzas

---
+++ script.py
@@ -0,0 +1,32 @@
+toppings = [
+    "pepperoni",
+    "pineapple",
+    "cheese",
+    "sausage",
+    "olives",
+    "anchovies",
+    "mushrooms",
+]
+print(toppings)
+
+prices = [2, 6, 1, 3, 2, 7, 2]
+print(prices)
+
+num_two_dollar_slices = prices.count(2)
+print(num_two_dollar_slices)
+
+num_pizzas = len(toppings)
+print(f"We sell {num_pizzas} different kinds of pizza!")
+
+pizza_and_prices = [
+    [2, "pepperoni"],
+    [6, "pineapple"],
+    [1, "cheese"],
+    [3, "sausage"],
+    [2, "olives"],
+    [7, "anchovies"],
+    [2, "mushrooms"],
+]
+print(pizza_and_prices)
+
+
